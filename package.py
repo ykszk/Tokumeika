299e9439102361313ec2f1f6dd3fa04cbe06561c
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2020-07-31 18:12:10 +0900

    Update packaging script
    * add version in the output filename

--- package.py
+++ package.py
@@ -1,17 +1,24 @@
 import shutil
 import os
+import sys
 import subprocess
 
+version_raw = sys.argv[1]
+version = version_raw.split('/')[-1]
 app_name = 'tokumeika-manager'
 
 if os.name == 'nt':
-    electron_app_name = app_name + '-win32-x64'
+    suffix = '-win32-x64'
 else:
-    electron_app_name = app_name + '-darwin-x64'
+    suffix = '-darwin-x64'
+
+electron_app_name = app_name + suffix
+output_filename = app_name + '-{}'.format(version) + suffix
 
 react_dir = 'build'
 python_dir = 'api/dist'
 config_dir = 'api/api/config'
+
 electron_dir = os.path.join('electron', 'build', electron_app_name)
 
 dst = electron_dir
@@ -24,4 +31,6 @@ print('Copy config files.')
 shutil.copytree(config_dir, os.path.join(dst, 'config'))
 
 print('Zip package')
-shutil.make_archive(electron_app_name, 'zip', dst)
+shutil.make_archive(output_filename, 'zip', dst)
+
+print('::set-output name=filename::{}.zip'.format(output_filename))
